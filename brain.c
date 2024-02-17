df24145e49ead63fb25b42ab857149b97d1d2cec
doomguy <artiomat13100@gmail.com>
2024-02-17 13:54:25 +0200

    MORE

--- brain.c
+++ brain.c
@@ -9,13 +9,21 @@
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
 
+#include <time.h>
+
 // How many neurons can a neuron see in one direction
-#define NEURON_SIGHT 4
+#define NEURON_SIGHT 8
 // How many bytes we shift the factor sum
 #define FACTOR_SHIFT 6
 
 // Decay speed affects how fast neuron firing decays in the brain, if the speed is too slow neurons may get into an equalibrium of firing, and will not escape it, so the faster the speed the more active the brain becomes.
-#define DECAY_SPEED 24
+#define DECAY_SPEED 32
+
+#define WND_W 256
+#define WND_H 128
+
+// 0=blue, 1=green, 2=red
+#define COLOR 2
 
 // Writable frame buffer, essentially blitting:
 // https://bbs.archlinux.org/viewtopic.php?id=225741
@@ -27,7 +35,6 @@ Atom wmdeletewindow_atom;
 GC xgc;
 Window xwnd;
 XImage* ximg;
-int wnd_w, wnd_h;
 
 unsigned char* ximg_data;
 
@@ -36,7 +43,7 @@ unsigned char* ximg_data;
 */
 typedef struct
 {
-  char factors[NEURON_SIGHT*NEURON_SIGHT*4]; // Making a rectangle around the neuron
+  char factors[(NEURON_SIGHT*2+1)*(NEURON_SIGHT*2+1)]; // Making a rectangle around the neuron
   u_char bias;
 } node_t;
 
@@ -63,12 +70,10 @@ int x_error_handler(Display * d, XErrorEvent * e)
   return 0;
 }
 
-int init_window_nodes(int w, int h)
+int init_window()
 {
   XSetErrorHandler(x_error_handler);
 
-  wnd_w = w;
-  wnd_h = h;
 	xdsp = XOpenDisplay(NULL);
 	if (!xdsp)
   {
@@ -97,7 +102,7 @@ int init_window_nodes(int w, int h)
 	xwnd = XCreateWindow(
 		xdsp,
 		RootWindow(xdsp, xscr),
-		0, 0, w, h,
+		0, 0, WND_W, WND_H,
 		0, // border width
 		24, // bit depth
 		InputOutput,
@@ -124,7 +129,7 @@ int init_window_nodes(int w, int h)
 	XFlush(xdsp); // Wait until window is visible.
 
 	// Image creation
-	char* data = malloc(w*32/8 * h); // 32 because of padding >:(
+	char* data = malloc(WND_W*32/8 * WND_H); // 32 because of padding >:(
 	if (!data)
   {
 		free_window();
@@ -139,9 +144,9 @@ int init_window_nodes(int w, int h)
 		ZPixmap,
 		0,
 		data,
-		w, h,
+		WND_W, WND_H,
 		32, // bitmap_pad, no clue, 32 is "if unsure".
-		w*32/8
+		WND_W*32/8
 	); // bytes per scanline, since we have padding of 32, we use 32 instead of 24.
 
 	if (!ximg)
@@ -153,9 +158,14 @@ int init_window_nodes(int w, int h)
 
   ximg_data = ximg->data;
 
-	XStoreName(xdsp, xwnd, "BRAIN");
-
-  nodes = malloc(w*h*sizeof(node_t));
+  // Limit window size
+  XSizeHints size_hints;
+  size_hints.flags = PMinSize | PMaxSize;
+  size_hints.min_width = WND_W;
+  size_hints.max_width = WND_W;
+  size_hints.min_height = WND_H;
+  size_hints.max_height = WND_H;
+  XSetWMNormalHints(xdsp, xwnd, &size_hints);
 
 	return 1;	
 }
@@ -171,7 +181,25 @@ void get_screen_size(int* w, int* h)
 // x y to index
 int xyi(int x, int y)
 {
-  return y*wnd_w + x;
+  if (x < 0)
+  {
+    x += WND_W;
+  }
+  else if (x >= WND_W)
+  {
+    x -= WND_W;
+  }
+  
+  if (y < 0)
+  {
+    y += WND_H;
+  }
+  else if (y >= WND_H)
+  {
+    y -= WND_H;
+  }
+
+  return y*WND_W + x;
 }
 
 void set_pixel(int i, char r, char g, char b)
@@ -195,28 +223,56 @@ static inline void decay_channel(unsigned char* c)
 
 void decay_img()
 {
-  for (int i = 0; i < wnd_w*wnd_h*4; i+=4)
+  for (int i = 0; i < WND_W*WND_H*4; i+=4)
   {
-    decay_channel(&ximg_data[i+0]);
-    decay_channel(&ximg_data[i+1]);
-    decay_channel(&ximg_data[i+2]);
+    if (ximg_data[i+0] == 0)
+    {
+      if (ximg_data[i+1] == 0)
+      {
+        decay_channel(&ximg_data[i+2]);
+      }
+      else
+      {
+        decay_channel(&ximg_data[i+1]);
+      }
+    }
+    else
+    {
+      decay_channel(&ximg_data[i+0]);
+    }
   }
 }
 
-// Returns how fired the pixel is, adds up channels, so make sure that just one channel is 1
-int get_fire(int i)
+// Returns how fired the neuron is from 0 to 255
+static inline int get_fire(int i)
 {
-  return ximg_data[i*4+0] + ximg_data[i*4+1] + ximg_data[i*4+2];
+  return ((int)ximg_data[i*4+2]+ximg_data[i*4+1]+ximg_data[i*4+0])/3;
 }
 
-// Returns if it fired, meaning >=255
-int get_if_fire(int i)
+// Returns if it fired, meaning it's above a specific threshold to be considered fired
+static inline int get_if_fire(int i)
 {
-  return (ximg_data[i*4+0] + ximg_data[i*4+1] + ximg_data[i*4+2]) >= 255;
+  return get_fire(i) >= 230;
+}
+
+// FULL POWER
+static inline void fire(int i)
+{
+  ximg_data[i*4+2] = 255;
+  ximg_data[i*4+1] = 255;
+  ximg_data[i*4+0] = 255;
+}
+
+// NO POWER
+static inline void unfire(int i)
+{
+  ximg_data[i*4+2] = 0;
+  ximg_data[i*4+1] = 0;
+  ximg_data[i*4+0] = 0;
 }
 
 // In monochrome
-void update_img()
+void put_img()
 {
 	XPutImage
   (
@@ -226,10 +282,8 @@ void update_img()
 		ximg,
 		0, 0,
 		0, 0,
-		wnd_w, wnd_h
+		WND_W, WND_H
 	);
-	
-	// XSync(xdsp, False);
 }
 
 void run_window()
@@ -255,8 +309,8 @@ void run_window()
 			break;
 			
 			case KeyPress:
-      for (int i = 0; i < wnd_w*wnd_h/4; i++)
-        set_pixel(rand()%(wnd_h*wnd_w), rand()%2?255:0, 0, 0);
+      for (int i = 0; i < WND_W*WND_H/4; i++)
+        set_pixel(rand()%(WND_H*WND_W), rand()%2?255:0, 0, 0);
       break;
 			
       case KeyRelease:
@@ -286,8 +340,15 @@ void run_window()
 
   if (mouse)
   {
-    printf("%i %i %i\n", mouse, mousex, mousey);
-    set_pixel(xyi(mousex, mousey), mouse==1?255:0, 0, 0);
+    int i = xyi(mousex, mousey);
+    if (mouse == 1)
+    {
+      fire(i);
+    }
+    else
+    {
+      unfire(i);
+    }
   }
 }
 
@@ -300,30 +361,53 @@ static inline int max(int x, int y)
   return x>y?x:y;
 }
 
-void run_nodes_to_img()
+void init_nodes()
+{
+  nodes = malloc(WND_W*WND_H*sizeof(node_t));
+
+
+  for (int i = 0; i < WND_W*WND_H; i++)
+  {
+    nodes[i].bias = rand();
+    for (int j = 0; j < sizeof(nodes[0].factors)/sizeof(nodes[0].factors[0]); j++)
+    {
+      nodes[i].factors[j] = rand();
+    }
+  }
+  for (int i = 0; i < WND_W*WND_H; i++)
+  {
+    if (rand()&1)
+      fire(i);
+    else
+      unfire(i);
+  }
+}
+
+void run_nodes_to_img(int seed)
 {
   int i = 0;
-  for (int y = 0; y < wnd_h; y++)
+  for (int y = 0; y < WND_H; y++)
   {
-    for (int x = 0; x < wnd_w; x++, i++)
+    for (int x = 0; x < WND_W; x++, i++)
     {
       // Run the node
       long sum = 0;
 
       // We need a j and a k to access both the real index in the ximg and the relative index in factors!
       int k = 0;
-      for (int v = max(y-NEURON_SIGHT, 0); v < min(y+NEURON_SIGHT+1, wnd_h); v++)
+      for (int v = y-NEURON_SIGHT; v <= y+NEURON_SIGHT; v++)
       {
-        for (int u = max(x-NEURON_SIGHT, 0); u < min(x+NEURON_SIGHT+1, wnd_w); u++, k++)
+        for (int u = x-NEURON_SIGHT; u <= x+NEURON_SIGHT; u++, k++)
         {
           int j = xyi(u, v);
 
-          if (j != i && get_fire(j)) // Make sure to igore THIS neuron
+          if (j != i && get_if_fire(j)) // Make sure to igore THIS neuron
           {
             sum+=nodes[i].factors[k];
           }
         }
       }
+      
       if (sum & (1<<(sizeof(int)*8-1))) // If sum is negative
       {
         sum = 0;
@@ -333,45 +417,50 @@ void run_nodes_to_img()
         sum >>= FACTOR_SHIFT;
       }
 
-      int fire = sum > (unsigned)nodes[i].bias;
+      int did_fire;
+      did_fire = sum > (long)nodes[i].bias;
 
       // Fire together? Wire together. Out of sync? Synapses shrink!
       k = 0;
-      for (int v = max(y-NEURON_SIGHT, 0); v < min(y+NEURON_SIGHT+1, wnd_h); v++)
+      for (int v = y-NEURON_SIGHT; v <= y+NEURON_SIGHT; v++)
       {
-        for (int u = max(x-NEURON_SIGHT, 0); u < min(x+NEURON_SIGHT+1, wnd_w); u++, k++)
+        for (int u = x-NEURON_SIGHT; u <= x+NEURON_SIGHT; u++, k++)
         {
           int j = xyi(u, v);
 
           if (j != i) // Make sure to igore THIS neuron
           {
-            if (fire & (get_fire(j) > 230)) // If both fired strengthen
+            if (did_fire & (get_fire(j) > 220)) // If both fired strengthen
             {
               nodes[i].factors[k] += nodes[i].factors[k]==255?0:1;
             }
-            else if (!fire & (get_fire(j) < 32))
+            else if (!did_fire & (get_fire(j) < 220))
             {
               nodes[i].factors[k] -= nodes[i].factors[k]==0?0:1;
             }
           }
         }
       }
-
+      
       // Going haywire? It will tire. Too quiet? 
       int last_fire = get_fire(i);
-      if (last_fire == 0 && !fire)
+      if (last_fire <= 0 && !did_fire)
       {
         nodes[i].bias -= nodes[i].bias==0?0:1;
       }
-      else if (last_fire >= 100 && fire)
+      else if (last_fire >= 128 && did_fire)
       {
         nodes[i].bias += nodes[i].bias==255?0:1;
       }
 
       // Put it on the image
-      if (fire)
+      if (did_fire)
       {
-        set_pixel(i, 255, 0, 0);
+        fire(i);
+      }
+      if (i == 1)
+      {
+        printf("%d\n", nodes[i].bias);
       }
     }
   }
@@ -379,28 +468,26 @@ void run_nodes_to_img()
 
 int main()
 {
-  srand(time(NULL));
-
-  init_window_nodes(256, 256);
-
-  for (int i = 0; i < wnd_w*wnd_h; i++)
-  {
-    nodes[i].bias = rand();
-    for (int j = 0; j < sizeof(nodes[0].factors)/sizeof(nodes[0].factors[0]); j++)
-    {
-      nodes[i].factors[j] = rand();
-    }
-  }
-  for (int i = 0; i < wnd_w*wnd_h*4; i+=4)
-  {
-    ximg_data[i+2] = rand();
-  }
+  init_window();
+  init_nodes();
   
-  for (unsigned i = 0; 1; i++)
+  int frames = 0;
+  time_t last_time = time(NULL), t;
+  char title[64];
+  while (1)
   {
     decay_img();
-    run_nodes_to_img();
+    run_nodes_to_img(frames);
     run_window();
-    update_img();
+    put_img();
+    
+    frames++;
+    if ((t = time(NULL)) > last_time)
+    {
+      last_time = t;
+      sprintf(title, "Brain : %dfps", frames);
+      frames = 0;
+      XStoreName(xdsp, xwnd, title);
+    }
   }
 }
